c91e9e047e6eb4888267046f186968bd603b03d8
echeverrigroup <steven.mobil2@gmail.com>
2026-06-08 04:47:11 -0400

    Update portal.js

--- portal/portal.js
+++ portal/portal.js
@@ -1,46 +1,43 @@
 import { supabase } from "../js/supabaseClient.js";
 import { requireAuth } from "./auth.js";
 
-
 async function init() {
 
     const user = await requireAuth();
 
     if (!user) return;
 
-    const userLabel =
-        document.getElementById("usuario-logueado");
+    document
+        .getElementById("logoutBtn")
+        .addEventListener("click", async () => {
 
-        document
-          .getElementById("logoutBtn")
-          .addEventListener("click", async () => {
-        
-              await supabase.auth.signOut();
-        
-              window.location.href = "/login.html";
-      });
+            await supabase.auth.signOut();
 
-        const { data: perfil, error } = await supabase
-      .from("usuarios")
-      .select(`
-          nombre,
-          apellido,
-          cargo,
-          empresa_id
-      `)
-      .eq("auth_user_id", user.id)
-      .single();
-    
+            window.location.href = "/login.html";
+        });
+
+    const { data: perfil, error } = await supabase
+        .from("usuarios")
+        .select(`
+            nombre,
+            apellido,
+            cargo
+        `)
+        .eq("auth_user_id", user.id)
+        .single();
+
+    console.log("USER:", user);
+    console.log("PERFIL:", perfil);
+    console.log("ERROR:", error);
+
     if (perfil) {
-    
+
         document.getElementById("user-name").textContent =
             `${perfil.nombre} ${perfil.apellido ?? ""}`;
-    
+
         document.getElementById("user-role").textContent =
             perfil.cargo ?? "Usuario";
     }
+}
 
-    document.getElementById("user-role").textContent =
-    `${perfil.cargo || "Usuario"} · ${perfil.empresas.nombre}`;
-    
 init();
