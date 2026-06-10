04eff1b22a58e3f3aa25ad0d4191f6cc1a709fbd
echeverrigroup <steven.mobil2@gmail.com>
2026-06-09 21:23:12 -0400

    Update portal.js

--- portal/portal.js
+++ portal/portal.js
@@ -41,6 +41,8 @@ async function init() {
        document.getElementById("user-role").textContent =
             `${perfil.cargo ?? "Usuario"} · ${perfil.empresas?.nombre ?? ""}`;
         }
+
+}
     
 
 init();
