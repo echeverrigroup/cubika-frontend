08b344034832ee534919687f63406a0382c602c9
echeverrigroup <steven.mobil2@gmail.com>
2026-06-09 22:04:53 -0400

    Add renderRubros function for Rubros page

---
+++ portal/pages/rubros.js
@@ -0,0 +1,22 @@
+export function renderRubros() {
+
+    return `
+        <div class="page-header">
+
+            <h1>Rubros</h1>
+
+            <button id="btnNuevoRubro">
+                + Nuevo Rubro
+            </button>
+
+        </div>
+
+        <div class="card">
+
+            <p>
+                Aquí se administrarán los rubros del sistema.
+            </p>
+
+        </div>
+    `;
+}
