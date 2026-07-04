4c6d82b4144a4c378813218a042239ce16d3ce37
echeverrigroup <steven.mobil2@gmail.com>
2026-07-04 10:09:04 -0400

    Add renderConstructionContratos function

---
+++ portal/pages/construction_contratos.js
@@ -0,0 +1,28 @@
+export function renderConstructionContratos() {
+
+    const content =
+        document.querySelector(".content");
+
+    content.innerHTML = `
+
+        <div class="page-header">
+
+            <h1>Contratos</h1>
+
+            <button class="btn-primary">
+                Generar Contrato
+            </button>
+
+        </div>
+
+        <div class="card">
+
+            <p>
+                Aquí estará el generador de contratos.
+            </p>
+
+        </div>
+
+    `;
+
+}
