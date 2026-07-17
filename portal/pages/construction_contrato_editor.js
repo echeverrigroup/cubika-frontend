c100c211b8cb52f62c6c9852ac3a61919ea398e7
echeverrigroup <steven.mobil2@gmail.com>
2026-07-17 10:38:52 -0400

    Update construction_contrato_editor.js

--- portal/pages/construction_contrato_editor.js
+++ portal/pages/construction_contrato_editor.js
@@ -577,7 +577,7 @@ function renderPaso3() {
 
                 <button
                     id="btnGenerarContrato"
-                    class="btn-cubika-green">
+                    class="page-header-button">
 
                     Aprobar y Generar
 
