0410fd0d322aec003256dab0a391f12a4b72106d
echeverrigroup <steven.mobil2@gmail.com>
2026-07-18 12:35:22 -0400

    Add document variable map for construction module

---
+++ portal/modules/construccion/documentVariableMap.js
@@ -0,0 +1,153 @@
+export const DOCUMENT_VARIABLE_MAP = {
+
+    EMPRESA:
+        "empresa.nombre",
+
+    RUT_EMPRESA:
+        "empresa.rut",
+
+    EMAIL_EMPRESA:
+        "empresa.email",
+
+    DIRECCION_EMPRESA:
+        "empresa.direccion",
+
+    COMUNA_EMPRESA:
+        "empresa.comuna.nombre",
+
+    REGION_EMPRESA:
+        "empresa.region.nombre",
+
+    REPRESENTANTE_LEGAL:
+        "empresa.representante_legal",
+
+    RUT_REPRESENTANTE:
+        "empresa.rut_representante",
+
+    DIRECCION_REPRESENTANTE:
+        "empresa.direccion_representante",
+
+    COMUNA_REPRESENTANTE:
+        "empresa.comuna_representante.nombre",
+
+    REGION_REPRESENTANTE:
+        "empresa.region_representante.nombre",
+
+
+
+
+    TRABAJADOR:
+        "trabajador.nombreCompleto",
+
+    NOMBRES:
+        "trabajador.nombres",
+
+    APELLIDO_PATERNO:
+        "trabajador.apellido_paterno",
+
+    APELLIDO_MATERNO:
+        "trabajador.apellido_materno",
+
+    RUT_TRABAJADOR:
+        "trabajador.rut",
+
+    SEXO:
+        "trabajador.sexo",
+
+    ESTADO_CIVIL:
+        "trabajador.estado_civil",
+
+    NACIONALIDAD:
+        "trabajador.nacionalidad",
+
+    FECHA_NACIMIENTO:
+        "trabajador.fecha_nacimiento",
+
+    EMAIL_TRABAJADOR:
+        "trabajador.email",
+
+    DIRECCION_TRABAJADOR:
+        "trabajador.direccion",
+
+    COMUNA_TRABAJADOR:
+        "trabajador.comuna.nombre",
+
+    REGION_TRABAJADOR:
+        "trabajador.region.nombre",
+
+    AFP:
+        "trabajador.afp",
+
+    SALUD:
+        "trabajador.salud",
+
+    BANCO:
+        "trabajador.banco",
+
+    TIPO_CUENTA:
+        "trabajador.tipo_cuenta",
+
+    NUMERO_CUENTA:
+        "trabajador.numero_cuenta",
+
+
+
+
+    OBRA:
+        "obra.nombre",
+
+    DESCRIPCION_OBRA:
+        "obra.descripcion",
+
+    DIRECCION_OBRA:
+        "obra.direccion",
+
+    COMUNA_OBRA:
+        "obra.comuna.nombre",
+
+    REGION_OBRA:
+        "obra.region.nombre",
+
+    FECHA_INICIO_OBRA:
+        "obra.fecha_inicio",
+
+    FECHA_TERMINO_OBRA:
+        "obra.fecha_termino",
+
+
+
+
+    CARGO:
+        "cargo.nombre",
+
+    DESCRIPCION_CARGO:
+        "cargo.descripcion",
+
+
+
+
+    NUMERO_CONTRATO:
+        "contrato.numero_contrato",
+
+    TIPO_CONTRATO:
+        "tipoContrato.nombre",
+
+    FECHA_INICIO:
+        "contrato.fecha_inicio",
+
+    FECHA_TERMINO:
+        "contrato.fecha_termino",
+
+    SUELDO:
+        "contrato.sueldo",
+
+    JORNADA:
+        "contrato.jornada",
+
+    CAUSAL_TERMINO:
+        "contrato.causal_termino",
+
+    OBSERVACIONES:
+        "contrato.observaciones"
+
+};
