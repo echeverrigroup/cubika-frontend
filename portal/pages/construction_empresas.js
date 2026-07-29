5c4dc8a981b4bc8a8a715438a477419a474552c6
echeverrigroup <steven.mobil2@gmail.com>
2026-07-29 05:22:56 -0400

    Update construction_empresas.js

--- portal/pages/construction_empresas.js
+++ portal/pages/construction_empresas.js
@@ -57,7 +57,7 @@ export async function renderConstructionEmpresas() {
                 tipoEmpresaActivo === "mandante"
                     ? "active"
                     : ""
-            }"🏢 Mandantes"
+            }"🏢 Mandantes
             
             </button>
         
