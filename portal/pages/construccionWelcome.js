49835b7d619eae4f32cd2ae3917c1a6a911cc73c
echeverrigroup <steven.mobil2@gmail.com>
2026-08-30 11:45:46 -0400

    Update construccionWelcome.js

--- portal/pages/construccionWelcome.js
+++ portal/pages/construccionWelcome.js
@@ -672,7 +672,7 @@ function getStepContent(step) {
                             <p>
                                 Cubika proporciona la infraestructura tecnológica
                                 para generar documentos a partir de las plantillas,
-                                datos y configuraciones que tu mismo proporcionas a la plataforma.
+                                datos y configuraciones que tu mismo gestionas la plataforma.
                             </p>
 
                             <p>
