414ce87930550a219534343e13063ca05eba61a1
echeverrigroup <steven.mobil2@gmail.com>
2026-08-30 13:39:48 -0400

    Update construccionWelcome.js

--- portal/pages/construccionWelcome.js
+++ portal/pages/construccionWelcome.js
@@ -538,7 +538,7 @@ function getStepContent(step) {
                             Los módulos de Cubika están diseñados para compartir información y evitar duplicaciones entre procesos.
 
                             <br>Por ejemplo, los datos gestionados en este <strong>Gestor Documental</strong> pueden utilizarse posteriormente en <strong>Asistencia y Remuneraciones</strong>.
-                            <br>Puedes incorporar nuevos módulos según las necesidades de tu operación.
+                            <br><br>Puedes incorporar nuevos módulos según las necesidades de tu operación.
                         </span>
 
                     </div>
